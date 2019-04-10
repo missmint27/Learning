9f3d42c0cfb0bb69fb969a195ef9a2d096cd0308
missmint27 <zyk493122273@163.com>
2019-04-10 11:15:12 +1000

    stack and queue

--- Python_and_algorithm/queueByList.py
+++ Python_and_algorithm/queueByList.py
@@ -1,5 +1,32 @@
-class queueByList(Object):
+class queueByList(object):
 	def __init__(self):
 		self.queue = []
 
-	
+	def put(self,item):
+		self.queue.append(item)
+		return self.queue
+
+	def get(self):
+		return print(self.queue.pop(0))
+
+	def size(self):
+		return print(len(self.queue))
+
+	def is_empty(self):
+		return print(self.queue == [])
+
+	def show(self):
+		return print(self.queue)
+
+
+
+if __name__ == '__main__':
+	q=queueByList()
+	q.put(2)
+	q.put(3)
+	q.size()
+	q.is_empty()
+	q.get()
+	q.show()
+	q.get()
+	q.is_empty()
